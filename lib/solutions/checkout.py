65741fcbeae057a7945df70cc0029808e4bdedc8
sbx_user1059 <sbx_user1059@ip-10-12-184-21.eu-west-2.compute.internal>
2018-04-27 19:03:02 +0000

    Fri Apr 27 19:03:02 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,5 +1,12 @@
 from collections import Counter
 
+PRICES = {
+    'A': [50, None, 130],
+    'B': [30, 45],
+    'C': [20],
+    'D': [15],
+}
+
 
 # noinspection PyUnusedLocal
 # skus = unicode string
