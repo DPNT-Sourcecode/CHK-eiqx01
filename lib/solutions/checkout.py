7798fcd4012b0c8134a5e0a49bd15f65bb6f4049
sbx_user1059 <sbx_user1059@ip-10-12-184-21.eu-west-2.compute.internal>
2018-04-27 19:53:17 +0000

    Fri Apr 27 19:53:17 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -39,6 +39,7 @@ def checkout(skus):
     for sku, count in c.items():
         if sku in GIFTS and count >= GIFTS[sku]['count']:
             gifts[GIFTS[sku]['sku']] += math.floor(count / GIFTS[sku]['count'])
+            
     for sku, count in c.items():
         if sku in gifts:
             count = max(0, count - gifts[sku])
